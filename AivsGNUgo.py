f8437ab48c2b9fe1a578f00da0f6a55fc4232801
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2024-11-14 22:08:48 +0100

    created AivsGnuGo.py

--- AivsGNUgo.py
+++ AivsGNUgo.py
@@ -1,9 +1,11 @@
 from policy_network import PolicyNetwork
-from goMCTS import MCTS
+from OnelayerMCTS import PreTrainedGoNetwork
+from OnelayerMCTS import MCTS
 from goEnv import GoGame
 import subprocess
 from time import sleep
 import sys
+import matplotlib.pyplot as plt
 
 coor_map = {
     'a': 0,
@@ -94,6 +96,23 @@ def apply_move(game, gnugo, move, is_gnugo_move=False):
             send_command(gnugo, "play white pass")
     game.render_in_terminal()
 
+def plot_winrate_over_games(gnugo_wins, goMCTS_wins, filename="win_ratio.png"):
+    total_games = gnugo_wins + goMCTS_wins
+    gnugo_winrate = gnugo_wins / total_games
+    goMCTS_winrate = goMCTS_wins / total_games
+
+    plt.figure(figsize=(8, 6))
+    plt.plot([0, 1], [gnugo_winrate, gnugo_winrate], label="GNU Go")
+    plt.plot([0, 1], [goMCTS_winrate, goMCTS_winrate], label="GoMCTS")
+    plt.xlabel("Game")
+    plt.ylabel("Win Ratio")
+    plt.title("Win Ratio over Games")
+    plt.legend()
+    plt.savefig(filename)
+    print(f"Plot saved to: {filename}")
+    return gnugo_winrate, goMCTS_winrate
+
+
     
 
 def main(simulations=1):
@@ -102,8 +121,11 @@ def main(simulations=1):
     goMCTS_wins = 0
     for _ in range(simulations):
         gnugo = run_gnugo()
-        network = PolicyNetwork("./models/PN_R3_C64.pt")
+        network = PreTrainedGoNetwork(checkpoint_path="./models/model.keras")
+        network.load_weights("./models/model.keras") 
+
         goMCTS = MCTS(network, num_simulations=5)
+        goPolicy = network
         game = GoGame(size=9)
 
         while game.isGameOver==False:
@@ -120,7 +142,7 @@ def main(simulations=1):
             apply_move(game, gnugo, goMCTS_move, is_gnugo_move=True)
 
             # Get move from GoMCTS
-            goMCTS_move = goMCTS.search(game)
+            goMCTS_move = goPolicy.select_move(game)
             print(f"GoMCTS move: {goMCTS_move}")
 
             
@@ -143,8 +165,10 @@ def main(simulations=1):
             gnugo_wins += 1
         else:
             goMCTS_wins += 1
-    print(f"GNU Go wins: {gnugo_wins}/{simulations}")
-    print(f"GoMCTS wins: {goMCTS_wins}/{simulations}")
+            
+    gnugo_winrate, goMCTS_winrate = plot_winrate_over_games(gnugo_wins, goMCTS_wins)
+    print(f"GNU Go win ratio: {gnugo_winrate:.2f}")
+    print(f"GoMCTS win ratio: {goMCTS_winrate:.2f}")
 
 if __name__ == "__main__":
     try:
